aeaef23ca3381da09d5edc8d309359457d71b8d5
Matankl <91688347+Matankl@users.noreply.github.com>
2023-04-12 19:02:27 +0300

    Update Ex4Main.java

--- Ex4/src/Exe/Ex4/Ex4Main.java
+++ Ex4/src/Exe/Ex4/Ex4Main.java
@@ -6,20 +6,13 @@ import Exe.Ex4.geo.Point2D;
 import Exe.Ex4.gui.Ex4;
 /**
  * A very simple main class for basic code for Ex4
- * 
- * t2: output:
- * GUIShape,0,true,1,Circle2D,3.0,4.0, 2.0
- * GUIShape,255,false,2,Circle2D,6.0,8.0, 4.0
- * 
- * @author boaz.ben-moshe
- *
  */
 public class Ex4Main {
 
 	public static void main(String[] args) {
 //		t1();
 		t2();
-//		t3(); // won't work "out of the box" - requires editing the code (save, load..)
+//		t3(); 
 	}
 	// Minimal empty frame (no shapes)
 	public static void t1() {
@@ -42,7 +35,7 @@ public class Ex4Main {
 		ex4.show();
 		System.out.print(ex4.getInfo());
 	}
-	// Loads a file from file a0.txt (Circles only).
+	// Loads a file from file a0.txt
 	public static void t3() {
 		Ex4 ex4 = Ex4.getInstance();
 		ShapeCollectionable shapes = ex4.getShape_Collection();
